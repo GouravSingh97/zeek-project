9c09b3f7cf371ec89e43c7c6b3043dc243ee95fe
Gourav Singh <gouravsinghchahar9784@gmail.com>
2025-06-02 19:42:17 +0530

    summary.sh

--- reports/summary.sh
+++ reports/summary.sh
@@ -2,12 +2,12 @@
 echo "Network Traffic Summary - $(date)"
 echo "----------------------"
 echo "Total Connections:"
-cat /home/futureweapon/zeek-project/logs/conn.log | wc -l
+cat /home/kali/zeek-project/logs/conn.log | wc -l
 echo "Top 5 Source IPs:"
-zeek-cut id.orig_h < /home/futureweapon/zeek-project/logs/conn.log | sort | uniq -c | sort -nr | head -5
+zeek-cut id.orig_h < /home/kali/zeek-project/logs/conn.log | sort | uniq -c | sort -nr | head -5
 echo "Top 5 DNS Queries:"
-zeek-cut query < /home/futureweapon/zeek-project/logs/dns.log | sort | uniq -c | sort -nr | head -5
+zeek-cut query < /home/kali/zeek-project/logs/dns.log | sort | uniq -c | sort -nr | head -5
 echo "Suspicious Notices:"
-cat /home/futureweapon/zeek-project/logs/notice.log | tail -5
+cat /home/kali/zeek-project/logs/notice.log | tail -5
 echo "Packet Loss Rate:"
-zeek-cut ts percent_lost < /home/futureweapon/zeek-project/logs/capture_loss.log | tail -5
+zeek-cut ts percent_lost < /home/kali/zeek-project/logs/capture_loss.log | tail -5
